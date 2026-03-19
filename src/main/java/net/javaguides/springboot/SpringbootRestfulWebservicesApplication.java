1fbcc1079627cb2723a75bda47fe5609a1f951ea
Shreya_Malavade <shreyacmalavade@gmail.com>
2026-03-19 23:12:21 +0530

    Updated the project

--- src/main/java/net/javaguides/springboot/SpringbootRestfulWebservicesApplication.java
+++ src/main/java/net/javaguides/springboot/SpringbootRestfulWebservicesApplication.java
@@ -1,18 +1,45 @@
 package net.javaguides.springboot;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Spring Boot Rest API Documentation",
+                description = "Spring Boot Rest API Documentation",
+                version = "v1.0",
+                contact = @Contact(
+                        name = "Yash",
+                        email = "yash@yash.com",
+                        url = "https://www.yash.com"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.yash.com/license"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Spring boot User Management Documentation",
+                url = "https://www.yash.com/user_magagement.html"
+        )
+
+)
 public class SpringbootRestfulWebservicesApplication {
 
-	@Bean
-	public ModelMapper modelMapper(){
-		return new ModelMapper();
-	}
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootRestfulWebservicesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRestfulWebservicesApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
--- src/main/java/net/javaguides/springboot/controller/UserController.java
+++ src/main/java/net/javaguides/springboot/controller/UserController.java
@@ -1,5 +1,8 @@
 package net.javaguides.springboot.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import net.javaguides.springboot.dto.UserDto;
@@ -15,6 +18,10 @@ import org.springframework.web.context.request.WebRequest;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Tag(
+        name = "CRUD Rest API for User Resource",
+        description = "CRUD REST APIs - Create User, Update User, Get User, Get All Users, Delete User"
+)
 @RestController
 @AllArgsConstructor
 @RequestMapping("api/users")
@@ -22,24 +29,61 @@ public class UserController {
 
     private UserService userService;
 
+    @Operation(
+            summary = "Create User Rest API",
+            description = "Create User REST API is used to save user in a database"
+    )
+
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status 201 CREATED"
+    )
     @PostMapping
     public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto user) {
         UserDto savedUser = userService.createUser(user);
         return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
     }
 
+
+    @Operation(
+            summary = "Get User By ID Rest API",
+            description = "Get User By ID REST API is used to get a single user from the database"
+    )
+
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
     @GetMapping("{id}")
     public ResponseEntity<UserDto> getUserById(@PathVariable("id") Long userId) {
         UserDto user = userService.getUserById(userId);
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Get All Users Rest API",
+            description = "Get all Users REST API is used to get all the user from the database"
+    )
+
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
     @GetMapping()
     public ResponseEntity<List<UserDto>> getAllUsers() {
         List<UserDto> users = userService.getAllUsers();
         return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Update User Rest API",
+            description = "Update User REST API is used to update a particular user in the database"
+    )
+
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
     @PutMapping("{id}")
     public ResponseEntity<UserDto> updateUser(@Valid @RequestBody UserDto user, @PathVariable("id") Long userId) {
         user.setId(userId);
@@ -47,6 +91,15 @@ public class UserController {
         return new ResponseEntity<>(updatedUser, HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Delete User Rest API",
+            description = "Delete User REST API is used to delete a particular user from the database"
+    )
+
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
     @DeleteMapping("{userId}")
     public ResponseEntity<String> deleteUser(@PathVariable Long userId) {
         return new ResponseEntity<>("User successfully deleted", HttpStatus.OK);
